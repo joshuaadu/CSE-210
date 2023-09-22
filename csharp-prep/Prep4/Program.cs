67de1ae168fb47de47fbaa4efaebe4ccdf41c6f9
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-09-21 23:24:07 -0600

    complet Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+        bool loop = true;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (loop)
+        {
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                int sum = 0;
+                foreach (int num in numbers)
+                {
+                    sum += num;
+                }
+
+                Console.WriteLine($"The sum is {sum}.");
+                float average = ((float)sum) / numbers.Count;
+                Console.WriteLine($"The average is {average}");
+                Console.WriteLine($"The largest number is {numbers.Max()}.");
+                Console.WriteLine($"The smallest number is {numbers.FindAll(number => number > 0).Min()}.");
+                Console.WriteLine($"The sorted list is: ");
+                numbers.Sort();
+                foreach (int num in numbers)
+                {
+                    Console.WriteLine($"{num} ");
+                }
+                loop = false;
+            }
+
+        }
     }
 }
