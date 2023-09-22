64af5b115d49923cf785f43d747cb0cb07dcc48e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-09-21 22:20:31 -0600

    complete C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        bool loop = true;
+        int guessCount = 0;
+        while (loop)
+        {
+
+            // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your guess? ");
+            int guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                loop = false;
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+            ++guessCount;
+            if (loop == false)
+            {
+
+                Console.WriteLine($"You have guessed {guessCount} times.");
+                Console.Write("Do you want to play again? (y/n): ");
+                string response = Console.ReadLine();
+                if (response == "y")
+                {
+                    loop = true;
+                    magicNumber = random.Next(1, 101);
+                }
+                // else
+                // {
+                //     loop = false;
+                // }
+                // loop = false;
+            }
+        }
+        Console.WriteLine($"You have guessed {guessCount} times.");
     }
 }
