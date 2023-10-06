6c254ec4770e3ca9bac978a65a034cf83b434b0f
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-10-05 19:09:05 -0600

    initialize journal class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        bool quit = false;
+        while (!quit)
+        {
+            PromptGeneratorMenu choice = PromptGenerator.GetMenuChoice();
+            switch (choice)
+            {
+                case PromptGeneratorMenu.Write:
+                    Entry entry = PromptGenerator.PromptEntry();
+                    journal.Add(entry);
+                    break;
+                case PromptGeneratorMenu.Display:
+                    journal.Display();
+                    break;
+                case PromptGeneratorMenu.Load:
+                    journal.Load();
+                    break;
+                case PromptGeneratorMenu.Save:
+                    journal.Save();
+                    break;
+                case PromptGeneratorMenu.Quit:
+                    quit = journal.Quit();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
     }
 }
