f503e48f41b446bf8c61f44fa64f7ddac80ff23f
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-10-12 23:15:47 -0600

    create Fraction class using encapsulation techniques

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionValue());
+        Console.WriteLine(f1.GetFractionString());
+        f1.SetTop(5);
+        f1.SetBottom(7);
+        Console.WriteLine(f1.GetFractionValue());
+        Console.WriteLine(f1.GetFractionString());
+
+        Fraction f2 = new Fraction(6);
+        Console.WriteLine(f1.GetFractionValue());
+        Console.WriteLine(f1.GetFractionString());
+        f2.SetTop(10);
+        f2.SetBottom(3);
+        Console.WriteLine(f1.GetFractionValue());
+        Console.WriteLine(f2.GetFractionString());
+
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f1.GetFractionValue());
+        Console.WriteLine(f1.GetFractionString());
+        f3.SetTop(5);
+        f3.SetBottom(9);
+        Console.WriteLine(f1.GetFractionValue());
+        Console.WriteLine(f3.GetFractionString());
+
     }
 }
