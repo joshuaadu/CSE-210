8a5d8c7678ca7f30a4e5d95f5313dcaf840891f9
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-11-09 23:19:44 -0700

    complete shapes polymorphism assignment

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,18 @@
+class Circle: Shape {
+    private double _radius;
+
+    public Circle(string color, double radius): base(color) {
+        _radius = radius;
+    }
+    public double GetRadius() {
+        return _radius;
+    }
+
+    public void SetRadius(double radius) {
+        _radius = radius;
+    }
+
+    public override double GetArea() {
+        return Math.PI * _radius * _radius;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 5);
+        Circle circle = new Circle("blue", 5);
+        Rectangle rectangle = new Rectangle("green", 5, 10);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The area of the {shape.GetColor()} shape is {shape.GetArea()}");
+        }
     }
 }
